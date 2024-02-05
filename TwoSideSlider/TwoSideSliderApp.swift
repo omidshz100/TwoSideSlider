69b0c547e35e77d69804b9e9bc51c8602bb0e4d0
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2024-02-05 13:17:10 +0100

    Initial Commit

---
+++ TwoSideSlider/TwoSideSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoSideSliderApp.swift
+//  TwoSideSlider
+//
+//  Created by Omid Shojaeian Zanjani on 05/02/24.
+//
+
+import SwiftUI
+
+@main
+struct TwoSideSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
